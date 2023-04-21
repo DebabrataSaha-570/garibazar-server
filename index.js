14f0dce18a13b670f28968f18fbe0a69dabee6f6
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2023-04-21 16:34:54 +0600

    addProduct api added

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 const app = express();
+require("dotenv").config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.orodex1.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("gariBazar");
+    const productsCollection = database.collection("products");
+
+    //POST API
+    app.post("/addProduct", async (req, res) => {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      console.log(result);
+      res.json(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Welcome to graribazar server");
